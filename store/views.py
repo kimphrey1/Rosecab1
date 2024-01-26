78a73b6bd0873b733bf170a35e3e55700f97eb8b
Daisy-Siime <daisyvega019@gmail.com>
2024-01-26 15:15:50 +0300

    search and filter working on products page

--- store/views.py
+++ store/views.py
@@ -6,13 +6,25 @@ from django.http import HttpResponseRedirect,HttpResponse
 
 def products(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
     # get the keywords from the search field to alter products list
     product_name = request.GET.get("product")
+    category_name = request.GET.get("category", "")
+    
+    # search function
     if product_name != "" and product_name is not None:
         products = products.filter(name__icontains=product_name)
     else:
         # if no item is searched input value is empty
         product_name = ""
+
+# filter category
+    if category_name:
+        selected_category = Category.objects.get(name=category_name)
+        products = Product.objects.filter(product_category=selected_category)
+        no_products_for_category = not products.exists()
+    else:
+        no_products_for_category = False
 
     # pagination
     paginator = Paginator(products, 12)
@@ -20,7 +32,9 @@ def products(request):
     products = paginator.get_page(page_number)
 
     # search_string is used to display input value as searched product
-    context = {"products": products, "search_string": product_name}
+    context = {"products": products, "search_string": product_name, 'categories': categories,
+        'selected_category': category_name, "no_products_for_category": no_products_for_category}
+    
     return render(request, "store/products.html", context=context)
 
 
@@ -60,10 +74,9 @@ def afterlogin(request):
  # filter category
     if category_name:
         selected_category = Category.objects.get(name=category_name)
-        products = Product.objects.filter(product_category=selected_category)
-        no_products_for_category = not products.exists()
+        pdts = Product.objects.filter(product_category=selected_category)
+        no_products_for_category = not pdts.exists()
     else:
-        products = Product.objects.all()
         no_products_for_category = False
 
     # pagination
